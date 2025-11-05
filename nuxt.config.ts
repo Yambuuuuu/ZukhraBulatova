f49b8d3f2dc89f45b52b52ceba1bc629107bf757
Yambu <angelika221106@gmail.com>
2025-11-05 10:56:04 +0300

    fixes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,14 +5,13 @@ export default defineNuxtConfig({
   // Основные настройки
   app: {
     head: {
-      title: 'ZB',
+      title: 'Zukhra Bulatova',
       htmlAttrs: {
         lang: 'ru'
       },
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { hid: 'description', name: 'description', content: '' }
       ],
       link: [
         { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
